d930224133bc27a31eb91a066d832e99716ca507
tsimard111 <tsimard123@gmail.com>
2015-08-08 19:20:46 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -11,16 +11,21 @@ smalldata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(smalldata$Date, smalldata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 ## Launch png graphics device
 png("plot4.png", width=480, height=480)
-## Plot lines
+## Set configuration of graphs
 par(mfrow = c(2, 2)) 
+## same as plot 2
 plot(datetime, smalldata$Global_active_power,
      type="l", xlab="", ylab="Global Active Power (kilowatts)")
+## New plot
 plot(datetime, smalldata$Voltage, type="l", xlab="datetime", ylab="Voltage")
+## Same as plot 3
 plot(datetime, smalldata$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, smalldata$Sub_metering_2, type="l", col="red")
 lines(datetime, smalldata$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        lty=1, lwd=2, col=c("black", "red", "blue"))
+## Last one!
 plot(datetime, smalldata$Global_reactive_power, 
      type="l", xlab="datetime", ylab="Global_reactive_power")
+## Turn off graphics device
 dev.off()
