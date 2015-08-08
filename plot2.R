c53d7851acae9f06ad043f3679aaeaf0bfb7b8c7
tsimard111 <tsimard123@gmail.com>
2015-08-08 19:13:23 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,10 +1,11 @@
+## Create directory and unzip files
 if(!file.exists("./epc")){dir.create("./epc")}
 download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "./epc/data.zip")
 unzip(zipfile = "./epc/data.zip", exdir = "./epc")
 ## Read data into R
 data <- read.table(header=TRUE, sep=";", stringsAsFactors=FALSE,
                    dec=".", na.strings="?",
-                   file="C://users/owner/desktop/rdir/rdir/epc/household_power_consumption.txt")
+                   file="./epc/household_power_consumption.txt")
 ## Subset to the desired date range
 smalldata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 ## Convert "Date" and "Time" into Posix
